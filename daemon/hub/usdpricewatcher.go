6776adef4506cb26a41ed2e8cd4a5210a5a8f64f
iquidus <luke@iquidus.io>
2022-01-26 14:19:49 -0500

    usdpricewatcher: use coingecko

--- daemon/hub/usdpricewatcher.go
+++ daemon/hub/usdpricewatcher.go
@@ -8,27 +8,33 @@ import (
 	"time"
 )
 
+type CoinGeckoResponse struct {
+	Ubiq struct {
+		Usd float64 `json:"usd"`
+	}
+}
+
 type CoinbaseSpotResponse struct {
 	Data struct {
-		Base   string  `json:"base"`
-		Amount string `json:"amount"`
+		Base     string `json:"base"`
+		Amount   string `json:"amount"`
 		Currency string `json:"currency"`
 	}
 }
 
 type USDPriceWatcher struct {
-	price float64
+	price      float64
 	priceMutex sync.RWMutex
 }
 
 func (u *USDPriceWatcher) StartWatching() {
 	client := &http.Client{Timeout: 10 * time.Second}
-	u.refreshCoinbasePrice(client)
+	u.refreshCoinGeckoPrice(client)
 
 	c := time.Tick(1 * time.Minute)
 	for now := range c {
 		_ = now
-		u.refreshCoinbasePrice(client)
+		u.refreshCoinGeckoPrice(client)
 	}
 }
 
@@ -40,25 +46,52 @@ func (u *USDPriceWatcher) GetPrice() float64 {
 
 func (u *USDPriceWatcher) refreshCoinbasePrice(client *http.Client) error {
 	r, err := client.Get("https://api.coinbase.com/v2/prices/ETH-USD/spot")
-    if err != nil {
+	if err != nil {
 		log.Errorln("Error getting coinbase price:", err)
-        return err
-    }
-    defer r.Body.Close()
+		return err
+	}
+	defer r.Body.Close()
 
-    response := CoinbaseSpotResponse{}
+	response := CoinbaseSpotResponse{}
 	err = json.NewDecoder(r.Body).Decode(&response)
-    if err != nil {
+	if err != nil {
 		log.Errorln("Error decoding coinbase response:", err)
-        return err
-    }
-	
+		return err
+	}
+
 	u.priceMutex.Lock()
 	u.price, err = strconv.ParseFloat(response.Data.Amount, 64)
 	u.priceMutex.Unlock()
-	
+
 	if err != nil {
 		log.Errorln("Error parsing coinbase price:", err)
+		return err
+	}
+
+	return nil
+}
+
+func (u *USDPriceWatcher) refreshCoinGeckoPrice(client *http.Client) error {
+	r, err := client.Get("https://api.coingecko.com/api/v3/simple/price?ids=ubiq&vs_currencies=USD")
+	if err != nil {
+		log.Errorln("Error getting coingecko price:", err)
+		return err
+	}
+	defer r.Body.Close()
+
+	response := CoinGeckoResponse{}
+	err = json.NewDecoder(r.Body).Decode(&response)
+	if err != nil {
+		log.Errorln("Error decoding coingecko response:", err)
+		return err
+	}
+
+	u.priceMutex.Lock()
+	u.price = response.Ubiq.Usd
+	u.priceMutex.Unlock()
+
+	if err != nil {
+		log.Errorln("Error parsing coingecko price:", err)
 		return err
 	}
 
